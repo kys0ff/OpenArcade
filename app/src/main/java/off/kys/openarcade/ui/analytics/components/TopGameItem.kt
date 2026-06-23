e6fa79c4c1c3ed925bacefb0842d51a869cd55c0
kys0ff <idk683120@gmail.com>
2026-06-23 12:57:31 +0100

    refactor(ui): fix progress bar scaling and optimize imports in TopGameItem

--- app/src/main/java/off/kys/openarcade/ui/analytics/components/TopGameItem.kt
+++ app/src/main/java/off/kys/openarcade/ui/analytics/components/TopGameItem.kt
@@ -4,7 +4,15 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -92,7 +100,8 @@ fun TopGameItem(
             ) {
                 Box(
                     modifier = Modifier
-                        .fillMaxSize(progressAnim)
+                        .fillMaxWidth(progressAnim)
+                        .fillMaxHeight()
                         .clip(CircleShape)
                         .background(
                             Brush.horizontalGradient(
